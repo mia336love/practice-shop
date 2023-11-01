18e7f29b36a256fe166db3abaa888d17381b5023
mia love <122865270+mia336love@users.noreply.github.com>
2023-11-01 13:15:12 +0300

    k 01/11

---
+++ practice-shop/src/Components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import styles from '../Header/Header.module.css';
+
+const Header = () =>{
+    return (
+        <div className={styles.header}>
+            <button className={styles.menuBtn}>
+                <img src="./icons/menu.svg" alt="menu" />
+            </button>
+            
+            <img className={styles.searchImg} src="./icons/search.svg" alt="search" />
+            <input type="text" className={styles.searchInp} placeholder="Поиск"/>
+            
+            <button className={styles.accountBtn}>
+                <img src="./icons/avatar.svg" alt="avatar" />
+            </button>
+            <button className={styles.favoritesBtn}>
+                <img src="./icons/heart.svg" alt="heart" />
+            </button>
+            <button className={styles.cartBtn}>
+                <img src="./icons/shopping-bag.svg" alt="shopping bag" />
+            </button>
+
+        </div>
+    )
+}
+
+export default Header
